4c229461a2994cc34a14755f47acaa6de033bc95
liutc10 <liutc10@hotmail.com>
2016-05-01 18:17:53 -0400

    Added save

--- PRT-master/script_mvrvm.m
+++ PRT-master/script_mvrvm.m
@@ -3,11 +3,25 @@ function script_mvrvm(outpath)
 % load data
 load ../data/MNIST.mat
 
-sigma = 2;
+[~, I1] =sort(Train_labels);
+[~, I2] =sort(Test_labels);
+Train = Train_images(:,I1);
+Test = Test_images(:,I2);
+Label = Train_labels(I1);
+GroundTruth = Test_labels(I2);
+
+for i = 0:9
+Traindata(:, (i*600+1):(i*600+600)) = Train(:, (i*6000+1):(i*6000+600));
+Testdata(:,(i*100+1):(i*100+100)) = Test(:, (i*1000+1):(i*1000+100));
+Labels((i*600+1):(i*600+600)) = Label((i*6000+1):(i*6000+600));
+Truth((i*100+1):(i*100+100)) = GroundTruth((i*1000+1):(i*1000+100));
+end
+
+sigma = 3;
 
 tic
-Train = prtDataSetClass(Train_images', Train_labels);
-Test = prtDataSetClass(Test_images', Test_labels);
+Train = prtDataSetClass(Traindata', Labels');
+Test = prtDataSetClass(Testdata', Truth');
 toc
 
 classifier = prtClassBinaryToMaryOneVsAll;
@@ -16,7 +30,10 @@ classifier.internalDecider = prtDecisionMap;
 
 kernel = prtKernelRbf('sigma', sigma);
 classifier.baseClassifier.kernels = kernel;
+
+fprintf('***BEGIN TRAINING***\n\n');
 classifier = classifier.train(Train);
+fprintf('***BEGIN TESTING***\n\n');
 classified = run(classifier, Test);
 
 fprintf( '\n***** WORKSPACE STORAGE *****\n\n' )
