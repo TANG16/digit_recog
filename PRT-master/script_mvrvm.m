12a38048554be389d10a67e31fdc1ff250042961
sx27 <sx27@duke.edu>
2016-05-01 17:41:43 -0400

    sampled script

--- PRT-master/script_mvrvm.m
+++ PRT-master/script_mvrvm.m
@@ -5,10 +5,15 @@ clear all, close all
 load ../data/MNIST.mat
 
 sigma = 2;
-
+Test_images_sample = Test_images(:,1:1000);
+Train_images_sample = Train_images(:,1:6000);
+Test_labels_sample = Test_labels(1:1000);
+Train_labels_sample = Train_labels(1:6000);
 tic
-Train = prtDataSetClass(Train_images', Train_labels);
-Test = prtDataSetClass(Test_images', Test_labels);
+% Train = prtDataSetClass(Train_images', Train_labels);
+% Test = prtDataSetClass(Test_images', Test_labels);
+Train = prtDataSetClass(Train_images_sample', Train_labels_sample);
+Test = prtDataSetClass(Test_images_sample', Test_labels_sample);
 toc
 
 classifier = prtClassBinaryToMaryOneVsAll;
